e5a1d06d2aa2e937cd95e4ee56d3ad7c8b773066
Your Name <Rimon0000>
2023-11-23 12:02:12 +0600

    get all users done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,27 +3,49 @@ import { userServices } from './user.service';
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = req.body;
-    const result = await userServices.createUserIntoDb(user);
+    const {user: userData} = req.body;
+    const result = await userServices.createUserIntoDb(userData);
 
     res.status(200).json({
       success: true,
       message: 'User created successfully',
       data: result,
     });
-  } catch (error) {
-    res.status(200).json({
-      success: false,
-      message: 'User not found',
-      error: {
-        code: 404,
-        description: 'User not found!',
-      },
-    });
-  }
+  } catch(error: any){
+    res.status(500).json({
+        success: false,
+        message: error.message || "something went wrong",
+        error: error
+    })
+}
   
 };
 
+//get all user
+const getAllUsers = async(req : Request, res : Response) =>{
+    try{
+        const result = await userServices.getAllUsersFromDb()
+        res.status(200).json({
+            success: true,
+            message: "Users fetched successfully!",
+            data: result
+        })
+    }catch (error) {
+        res.status(200).json({
+          success: false,
+          message: 'User not found',
+          error: {
+            code: 404,
+            description: 'User not found!',
+          },
+        });
+      }
+}
+
+
+
 export const userControllers = {
   createUser,
+  getAllUsers,
+  
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,15 +2,21 @@ import { TUser } from "./user.interface";
 import { User } from "./user.model";
 
 
+//create user
 const createUserIntoDb = async (userData: TUser) => {
-  
     const result = await User.create(userData);
+    return result;
+  };
 
+  //get all user
+const getAllUsersFromDb = async () => {
+    const result = await User.find();
     return result;
   };
 
   export const userServices = {
     createUserIntoDb,
-    
+    getAllUsersFromDb
+
     
   }
